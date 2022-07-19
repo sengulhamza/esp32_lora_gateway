68b4ab4e81a3885b99de950ae113bacfc083cc27
sengulhamza <sengulhamza@icloud.com>
2022-07-19 18:52:59 +0300

    device provisioning packet added

--- src/app/inc/app/lora_manager.h
+++ src/app/inc/app/lora_manager.h
@@ -8,10 +8,13 @@
 extern "C" {
 #endif
 
+#define LORA_PACKET_ID_PROVISING 0xE1
+
 typedef struct {
-    uint8_t node_id;
-    char    data[250];
+    uint8_t packet_id;
+    uint8_t data[58];
     uint16_t data_len;
+    uint8_t end_of_frame;
 } lora_tx_packet;
 
 esp_err_t lora_process_start(void);
