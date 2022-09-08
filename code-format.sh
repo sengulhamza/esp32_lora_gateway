02d43367cbd9dd24c8d00871156975fdc73d4ac8
sengulhamza <sengulhamza@icloud.com>
2022-09-08 20:00:41 +0300

    version increased to 0.1.0

--- code-format.sh
+++ code-format.sh
@@ -1,1 +1,16 @@
-./tools/format.sh --recursive "src/core/*.c" "src/core/*.h" "src/app/*.c" "src/app/*.h" "src/app/*.hpp" "src/app/*.cpp"
+astyle \
+	--style=otbs \
+	--attach-namespaces \
+	--attach-classes \
+	--indent=spaces=4 \
+	--convert-tabs \
+	--align-pointer=name \
+	--align-reference=name \
+	--keep-one-line-statements \
+	--pad-header \
+	--pad-oper \
+	--pad-comma \
+	--unpad-paren \
+	--suffix=none \
+	"$@" \
+    --recursive "src/core/*.c" "src/core/*.h" "src/app/*.c" "src/app/*.h" "src/app/*.hpp" "src/app/*.cpp"
