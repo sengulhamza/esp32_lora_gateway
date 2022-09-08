c3908835e8b8a05c32e8d61ee0288b5cd0d16b80
sengulhamza <sengulhamza@icloud.com>
2022-09-08 18:22:05 +0300

    device mode managing and store

--- src/app/inc/app/app_types.h
+++ src/app/inc/app/app_types.h
@@ -3,9 +3,15 @@
 
 #include <stdint.h>
 
+typedef enum {
+    APP_DEVICE_IS_MASTER,
+    APP_DEVICE_IS_CLIENT
+} app_device_type;
+
 typedef struct {
     const char *dev_serial;
     const char *dev_model;
+    app_device_type device_type;
 } app_params_t;
 
 extern app_params_t app_params;
